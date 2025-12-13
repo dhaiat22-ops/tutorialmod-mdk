12b7b32e0d03013a01a786f726d0cc3ebf1d849f
David Haiat <dhaiat22@gmail.com>
2025-12-14 01:09:10 +0200

    Very Important Changes

--- src/main/java/net/david/tutorialmod/TutorialMod.java
+++ src/main/java/net/david/tutorialmod/TutorialMod.java
@@ -1,6 +1,9 @@
 package net.david.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.david.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -38,6 +41,8 @@ public class TutorialMod
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -53,6 +58,10 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
 
     }
 
